0f99053345676d171ee1617b5683064e0c3141e2
Mitesh-Saste <miteshsaste35@gmail.com>
2025-05-10 18:51:32 +0530

    update feature/login branch

---
+++ app/login.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, request
+
+login_bp = Blueprint('login', __name__)
+
+@login_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    return {"status": "success", "user": username}
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from login import login_bp
 app = Flask(__name__)
+app.register_blueprint(login_bp)
 
 @app.route('/')
 def home():
